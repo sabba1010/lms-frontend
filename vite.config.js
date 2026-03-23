a69f74511e8e94d673de197af6af06955e6eceae
Sabba Hossain <sabbahossain@gmail.com>
2026-03-23 09:55:39 +0600

    reload issue scom file

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
         target: 'http://localhost:5000',
         changeOrigin: true,
       },
-      '/scorm': {
+      '/scorm/': {
         target: 'http://localhost:5000',
         changeOrigin: true,
       }

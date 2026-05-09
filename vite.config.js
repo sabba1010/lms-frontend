86e2f99a591a6de371ae50d2dd7e429794c36186
Sabba Hossain <sabbahossain@gmail.com>
2026-05-09 10:20:20 +0600

    sadasdas

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000/',
+        target: 'http://104.250.128.20',
         changeOrigin: true,
       },
+
       '/scorm': {
-        target: 'http://localhost:5000',
+        target: 'http://104.250.128.20',
         changeOrigin: true,
       },
+
       '/uploads': {
-        target: 'http://localhost:5000',
+        target: 'http://104.250.128.20',
         changeOrigin: true,
       }
     }
